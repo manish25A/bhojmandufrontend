f790fb6d684eb008f169bca523fcab4cb528dc30
Manish <manysh23@gmail.com>
2021-03-18 23:39:44 +0545

    adding header and style

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,69 +1,66 @@
-import React, {Component} from "react";
-import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
-import {Button, Form, FormControl, Nav, Navbar} from "react-bootstrap";
+import React, {Component, useEffect, useState} from "react";
+import {BrowserRouter as Router, Link, NavLink, Route, Switch} from "react-router-dom";
 import Login from "../Body/Login";
 import Footer from "../Footer/footer"
 import Register from "../Body/Register";
+import {AppBar, Hidden, Toolbar, Typography,} from '@material-ui/core';
+import {useStyles} from './style';
+import {CategoryOutlined, ExpandMoreOutlined, HomeOutlined, Search, Storefront} from '@material-ui/icons';
+import {useDispatch, useSelector} from 'react-redux';
 
-class Header extends Component<any> {
-    render() {
-        return (
+export const Header = () => {
+    const dispatch = useDispatch();
+    // const categories = useSelector((state) => state.categories.categories);
+    const user = useSelector((state) => state.users.user);
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [openSidebar, setOpenSidebar] = useState(false);
+    const cartItems = user?.cart?.items?.reduce((total, value) => {
+        return value.quantity + total;
+    }, 0);
 
-            <Router>
-                <div>
-                    <Navbar bg="dark" variant="dark">
-                        <Navbar.Brand href="/">Home</Navbar.Brand>
-                        <Nav className="mr-auto">
-                            <Nav.Link href="/body/Register">Register</Nav.Link>
-                            <Nav.Link href="/login">Features</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        </Nav>
-                        <Form inline>
-                            <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
-                            <Button variant="outline-info">Search</Button>
-                        </Form>
-                    </Navbar>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/body/register">Register</Link>
-                        </li>
-                        <li>
-                            <Link to="/login">Login</Link>
-                        </li>
-                        <li>
-                            <Link to="/footer/home">
-                                Footer
-                            </Link>
-                        </li>
-                    </ul>
+    return (
+                        <AppBar
+                            position="sticky"
+                            className={classes.root}
+                            style={{backgroundColor: 'white'}}
+                        >
+                            <Toolbar>
+                                <Typography
+                                    className={classes.heading}
+                                    variant="h4"
+                                    component={NavLink}
+                                    to="/"
+                                    exact
+                                >
+                                    BhojMandu
+                                </Typography>
 
-                    <hr/>
+                                <Hidden smDown>
+                                    <Search className={classes.input}/>
+                                </Hidden>
 
-                    {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-                    <Switch>
-                        <Route path="/footer/home">
-                            <Footer/>
-                        </Route>
-                        <Route path="/body/register">
-                            <Register/>
-                        </Route>
-                        <Route path="/login">
-                            <Login/>
-                        </Route>
-                    </Switch>
-                </div>
-            </Router>
-        )
-    }
-}
+                                <Hidden mdDown>
+                                    <Typography
+                                        component={NavLink}
+                                        activeClassName={classes.activeLink}
+                                        exact
+                                        to="/"
+                                        className={classes.navItems}
+                                    >
+                                        <HomeOutlined/> Home
+                                    </Typography>
 
-export default Header
+                                    <Typography
+                                        component={NavLink}
+                                        activeClassName={classes.activeLink}
+                                        exact
+                                        to="/shop"
+                                        className={classes.navItems}
+                                    >
+                                        <Storefront/> Shop
+                                    </Typography>
+                                </Hidden>
+                            </Toolbar>
+                        </AppBar>
+    )
