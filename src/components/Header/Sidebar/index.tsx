ed45f4181183a648ce12780dc1b1f914ec181e54
Manish <manysh23@gmail.com>
2021-03-30 23:46:33 +0545

    updated design on index and sidebar

--- src/components/Header/Sidebar/index.tsx
+++ src/components/Header/Sidebar/index.tsx
@@ -1,27 +1,21 @@
-import React from 'react'
-import {
-    SidebarContainer,
-    Icon,
-    CloseIcon,SidebarWrapper,SidebarMenu,
-    SidebarLink
-}
-from './SidebarElements'
+import React from 'react';
+import { SidebarContainer, Icon, CloseIcon } from './SidebarElements';
 const Sidebar = () => {
-    return (
- <SidebarContainer>
-    <Icon>
-       <CloseIcon/>
-    </Icon>
-    <SidebarWrapper>
-    <SidebarMenu>
-            <SidebarLink to="/about"> About</SidebarLink>
-            <SidebarLink to="/products">Product</SidebarLink>
-            <SidebarLink to="/discover">Discover</SidebarLink>
-            <SidebarLink to="/register">Register</SidebarLink>
-    </SidebarMenu>
-    </SidebarWrapper>
-</SidebarContainer>
-    )
-}
+	return (
+		<SidebarContainer>
+			<Icon>
+				<CloseIcon />
+			</Icon>
+			<SidebarWrapper>
+				<SidebarMenu>
+					<SidebarLink to='/about'>About</SidebarLink>
+					<SidebarLink to='/products'>Product</SidebarLink>
+					<SidebarLink to='/discover'>Discover</SidebarLink>
+					<SidebarLink to='/register'>Register</SidebarLink>
+				</SidebarMenu>
+			</SidebarWrapper>
+		</SidebarContainer>
+	);
+};
 
 export default Sidebar;
